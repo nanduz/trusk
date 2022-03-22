27da1e23ee4a34ac8c7fadb31f87c90ec9ecdabf
nanduz <64654705+nanduz@users.noreply.github.com>
2022-03-22 15:38:14 -0300

    Update headers.sol

--- headers.sol
+++ headers.sol
@@ -1,5 +1,5 @@
 //SPD-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts@4.4.1/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts@4.4.1/access/Ownable.sol";
+import "@openzeppelin/contracts@4.5.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.5.0/access/Ownable.sol";
